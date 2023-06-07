04f8d101cde974ae25c3c42f68682df582df4894
aburahan8721 <aburaihan8721@gmail.com>
2023-06-07 13:39:40 +0600

    database connection done

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
 const express = require("express");
+require("dotenv").config();
 const app = express();
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// connection string
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.eujox13.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+// database connection
+const run = async () => {
+  try {
+    const toyTopiaDb = client.db("toyTopiaDb");
+    const toyCollection = toyTopiaDb.collection("toys");
+    console.log("database connected");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close()
+  }
+};
+
+run().catch((err) => {
+  console.log(err.message);
+});
 
 // home route
 app.get("/", async (req, res) => {
